4f9df370ddc79ab2aabbca8b44c346ff3abded9d
Maria Kulgava <mashak@propertyvista.com>
2025-01-21 16:25:17 +0200

    test commit

--- src/test/java/tests/UnixNavigationTest.java
+++ src/test/java/tests/UnixNavigationTest.java
@@ -11,6 +11,7 @@ import runner.BaseRunner;
 import utils.TestDataReaderUtil;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
 import static runner.DriverManager.getBaseUrl;
 
 public class UnixNavigationTest extends BaseRunner {
@@ -30,16 +31,19 @@ public class UnixNavigationTest extends BaseRunner {
         subCategoryName = testData.subcategory;
 
         open(getBaseUrl());
+        sleep(5000);
     }
 
     @Test
     public void testUnixNavigation() {
         // Open the homepage
         accentHeaderComponent.navigateToHomePage();
+        sleep(5000);
         homePage.assertHomePageTitle();
 
         // Select the DevOps category and choose the Unix sub-category
         headerShowComponent.navigateToSubcategory(categoryName, subCategoryName);
+        sleep(5000);
 
         // Verify successful navigation
         unixTutorialPage.assertUnixTutorialTitle()
